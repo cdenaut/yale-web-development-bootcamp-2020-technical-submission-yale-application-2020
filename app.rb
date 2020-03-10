31227ef81c1e2b3283c6f1e97ee11d5b588b4d26
Courtney DeNaut <courtney.denaut@yale.edu>
2020-03-10 22:41:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Courtney DeNaut"
+age = 20
+favorite_food = "French Fries"
+height_in_inches = 69
+height_message = height_in_inches > AVERA
 
 ## Do not modify below this line!
 
